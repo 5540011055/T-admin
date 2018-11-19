259cfd1cc259304b8020c7268cbbee765297ce88
5540011055 <izudsuarez@gmail.com>
2018-11-19 12:09:52 +0700

    ปปป

--- assets/custom.js
+++ assets/custom.js
@@ -1,39 +1,76 @@
+function logOut() {
+  modal.show();
+  $('#signout-alert-dialog').hide();
+  /*$.removeCookie('detect_user', {
+   path: '/'
+   });
+   $.removeCookie('detect_userclass', {
+   path: '/'
+   });
+   $.removeCookie('detect_username', {
+   path: '/'
+   });*/
+  //                    clearCookieAll();
+  /*ons.notification.alert({
+   message: 'ออกจากระบบสำเร็จ',
+   title: "สำเร็จ",
+   buttonLabel: "ปิด"
+   })
+   .then(function() {
+   
+   
+   });*/
+  clearCookieAll();
+  deleteTagOs("Test Text");
+  deleteTagIOS(class_user, username);
+
+  setTimeout(function () {
+    window.location = "../TShare_new/material/login/index.php";
+  }, 2000);
+}
+
+function callpop() {
+  console.log(appNavigator)
+  appNavigator.popPage()
+}
+
 function filterShopTrans(id) {
-      $('.ex').removeClass('active-btn_shoptype');
-      $('#' + id).addClass('active-btn_shoptype');
-      load_list_trans_shop();
-    }
+  $('.ex').removeClass('active-btn_shoptype');
+  $('#' + id).addClass('active-btn_shoptype');
+  load_list_trans_shop();
+}
 
-    function shopJob() {
-      fn.pushPage({
-        'id': 'shop.html',
-        'title': 'งานส่งแขก'
-      }, 'slide-ios');
-      load_list_trans_shop();
-    }
+function shopJob() {
+  fn.pushPage({
+    'id': 'shop.html',
+    'title': 'งานส่งแขก'
+  }, 'slide-ios');
+  load_list_trans_shop();
+}
 
-    function load_list_trans_shop() {
-      $('#body_shop').html(progress_circle);
-      var filter_type = $('#filter_type_trans_shop').val();
-      var url = "shop/list_trans?filter_type=" + filter_type;
-      console.log(url);
-      $.post(url, function (ele) {
-        $('#body_shop').html(ele);
-      });
-    }
+function load_list_trans_shop() {
+  $('#body_shop').html(progress_circle);
+  var filter_type = $('#filter_type_trans_shop').val();
+  var url = "shop/list_trans?filter_type=" + filter_type;
+  console.log(url);
+  $.post(url, function (ele) {
+//        console.log(ele);
+    $('#body_shop').html(ele);
+  });
+}
 
-    function openManageShopTrans(id) {
-      fn.pushPage({
-        'id': 'popup1.html',
-        'title': 'งานส่งแขก'
-      }, 'lift-ios');
+function openManageShopTrans(id) {
+  fn.pushPage({
+    'id': 'popup1.html',
+    'title': 'งานส่งแขก'
+  }, 'lift-ios');
 //      $('#body_popup1').html(progress_circle);
-      $.post("shop/detail_trans?order_id=" + id, function (ele) {
-        $('#body_popup1').html(ele);
-      });
+  $.post("shop/detail_trans?order_id=" + id, function (ele) {
+    $('#body_popup1').html(ele);
+  });
 
-    }
-    
+}
+
 function calcost(cost) {
   if (cost == "") {
     cost = 0;
@@ -52,20 +89,20 @@ function calcost(cost) {
   $('#taxi_cost').val(total_price_taxi);
 }
 
-function confirmTransferMoneyShop(){
+function confirmTransferMoneyShop() {
   var dialog = document.getElementById('confirm-trans_shop-dialog');
 
   if (dialog) {
     dialog.show();
   } else {
-    ons.createElement('confirm-trans_shop.html', { append: true })
-      .then(function(dialog) {
-        dialog.show();
-      });
+    ons.createElement('confirm-trans_shop.html', {append: true})
+            .then(function (dialog) {
+              dialog.show();
+            });
   }
 }
 
-function submitTransShop(){
+function submitTransShop() {
   modal.show();
   var data = new FormData($('#form_acc_trans_shop')[0]);
   data.append('slip_trans', $('#img_upload')[0].files[0]);
@@ -82,15 +119,18 @@ function submitTransShop(){
     type: 'post',
     success: function (res) {
       console.log(res);
-       modal.hide();
+      modal.hide();
 
       if (res.data.result == true) {
 //       
 //        filterShopList();
 //        sendSocket(res.update.order_id);
 //        modal.hide();
+        sendSocket(res.update.order_id);
+        callpop();
+        load_list_trans_shop();
         $.ajax({
-          url: base_url+"send_onesignal/transfer_shop_completed?id=" + res.update.order_id, // point to server-side PHP script 
+          url: base_url + "send_onesignal/transfer_shop_completed?id=" + res.update.order_id, // point to server-side PHP script 
           dataType: 'json', // what to expect back from the PHP script, if anything
           type: 'post',
           success: function (res) {
@@ -134,8 +174,8 @@ function performClick(elemId) {
   }
 }
 
-function hideAlertDialog(id){
+function hideAlertDialog(id) {
   document
-    .getElementById(id)
-    .hide();
+          .getElementById(id)
+          .hide();
 }
